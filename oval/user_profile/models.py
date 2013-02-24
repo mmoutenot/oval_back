1efdf2537f6cb30985885331da8e17c7a376e664
Marshall <mmoutenot@gmail.com>
2013-02-23 21:16:55 -0500

    restructure dir

--- oval/user_profile/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class UserProfile (models.Model):
-    user = models.ForeignKey(User, related_name='profile')
-    first_name = models.CharField(max_length=30)
-    last_name  = models.CharField(max_length=30)
-
-
